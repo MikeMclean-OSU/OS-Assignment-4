bc9d530b116895250d43a65868b30ffe13dcd10c
MikeMclean-OSU <137909998+MikeMclean-OSU@users.noreply.github.com>
2025-03-03 03:02:40 +0000

    Almost complete

--- mcleamic_assignment4.c
+++ mcleamic_assignment4.c
@@ -10,6 +10,9 @@
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
 
+int fg_process = -1;
+int sigtstp_flag = 0; //0 = not foregorund only mode, 1 = foreground only mode
+
 
 struct command_line
 {
@@ -48,16 +51,25 @@ struct command_line *parse_input()
 	return curr_command;
 }
 
-// void handle_SIGCHLD(int signo){
-// 	int status;
-// 	pid_t id = waitpid(-1, &status, WNOHANG);
+void handle_SIGINT(int signo){
+	if (fg_process > -1){
+		kill(fg_process, SIGTERM);
+		fg_process = -1;
+	}
+}
 
-// 	while (id > 0){
-// 		printf("\nbackground pid %d is done: exit value %d\n", id, status);
-// 		fflush(stdout);
-// 		id = waitpid(-1, &status, WNOHANG);
-// 	}
-// }
+void handle_SIGTSTP(int signo){
+	char* exit_message = "\nExiting foreground-only mode\n";
+	char* enter_message = "\nEntering foreground-only mode (& is now ignored)\n";
+	
+	if(sigtstp_flag){
+		sigtstp_flag = 0;
+		write(STDOUT_FILENO, exit_message, 29);
+	}else{
+		sigtstp_flag = 1;
+		write(STDOUT_FILENO, enter_message, 49);
+	}
+}
 
 int main()
 {
@@ -65,26 +77,40 @@ int main()
 	pid_t id;
 	int childStatus;
 	struct command_line *curr_command;
+	
+	//Signal handler for SIGTERM
 	struct sigaction SIGTERM_action = {0};
-
 	SIGTERM_action.sa_handler = SIG_DFL;
 	sigfillset(&SIGTERM_action.sa_mask);
+	SIGTERM_action.sa_flags = 0;
 	sigaction(SIGTERM, &SIGTERM_action, NULL);
 
-	// struct sigaction SIGCHLD_action = {0};
-
-	// SIGCHLD_action.sa_handler = handle_SIGCHLD;
-	// sigemptyset(&SIGCHLD_action.sa_mask);
-	// SIGCHLD_action.sa_flags = SA_RESTART;
-	// sigaction(SIGCHLD, &SIGCHLD_action, NULL);
+	//Signal handler for SIGINT
+	struct sigaction SIGINT_action = {0};
+	SIGINT_action.sa_handler = handle_SIGINT;
+	sigfillset(&SIGINT_action.sa_mask);
+	SIGINT_action.sa_flags = 0;
+	sigaction(SIGINT, &SIGINT_action, NULL);
+	
+	//Signal handler for SIGTSTP
+	struct sigaction SIGTSTP_action = {0};
+	SIGTSTP_action.sa_handler = handle_SIGTSTP;
+	sigfillset(&SIGTSTP_action.sa_mask);
+	SIGTSTP_action.sa_flags = 0;
+	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
 
 	while(true)
 	{
 		id = waitpid(-1, &status, WNOHANG);
 		while (id > 0){
-			printf("background pid %d is done: exit value %d\n", id, WEXITSTATUS(status));
-			fflush(stdout);
+			if (WIFSIGNALED(status)){
+				printf("background pid %d is done: terminated by signal %d\n", id, WTERMSIG(status));
+				fflush(stdout);
+			}else{
+				printf("background pid %d is done: exit value %d\n", id, WEXITSTATUS(status));
+				fflush(stdout);
+			}
 			id = waitpid(-1, &status, WNOHANG);
 		}
 
@@ -114,8 +140,13 @@ int main()
 			}
 		
 		}else if (!strcmp(token, "status")){
-			printf("exit value %d\n", WEXITSTATUS(childStatus));
-			fflush(stdout);
+			if(WIFSIGNALED(childStatus)){
+				printf("terminated by signal %d\n", WTERMSIG(childStatus));
+				fflush(stdout);
+			}else{
+				printf("exit value %d\n", WEXITSTATUS(childStatus));
+				fflush(stdout);
+			}
 			continue;
 		}
 
@@ -126,7 +157,9 @@ int main()
 			exit(1);
 		}else if(childpid == 0){
 			//child case
-
+			if (!curr_command->is_bg){
+				fg_process = getpid();
+			}
 			//Redirect output if file is given
 			if (curr_command->output_file){
 				int fd_out = open(curr_command->output_file, O_WRONLY | O_CREAT, 0640);
@@ -142,26 +175,33 @@ int main()
 				int redirect_in = dup2(fd_in, 0);
 			}
 			//Redirect output of background process if no file specified
-			if (curr_command->is_bg && !curr_command->output_file){
+			if (curr_command->is_bg && !curr_command->output_file && !sigtstp_flag){
 				int fd_out = open("/dev/null", O_WRONLY);
 				int redirect_out = dup2(fd_out, 1); 
 			}
 			//Redirect input of background process if no file specified
-			if (curr_command->is_bg && !curr_command->input_file){
+			if (curr_command->is_bg && !curr_command->input_file && !sigtstp_flag){
 				int fd_in = open("/dev/null", O_RDONLY);
 				int redirect_out = dup2(fd_in, 0); 
 			}
-
+			
+			
 			execvp(token, curr_command->argv);
 			perror("Error");
 			exit(2);
+			
 		}else{
-			if (curr_command->is_bg){
+			//Parent case
+			if (curr_command->is_bg && !sigtstp_flag){
 				printf("Background pid is %d\n", childpid);
 				fflush(stdout);
 				childpid = waitpid(childpid, &childStatus, WNOHANG);
 			}else{
 				childpid = waitpid(childpid, &childStatus, 0);
+				if (WIFSIGNALED(childStatus)){
+					printf("terminated by signal %d\n", WTERMSIG(childStatus));
+					fflush(stdout);
+				}
 			}
 		}	
 		
